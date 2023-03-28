ea66a7aa2755cb8e2f6ea5303a24863dd3637ced
adricarolinaq <adrianard1997@gmail.com>
2023-03-28 14:11:56 -0500

    triangulo

--- TRIANGULO.py
+++ TRIANGULO.py
@@ -1,5 +1,5 @@
-base=float(input("ingresa base:"))
-altura=float(input("ingrese altura:"))
+base=int(input("ingresa base:"))
+altura=int(input("ingrese altura:"))
 area=(base * altura)/2
 print("area es:",area)
 
